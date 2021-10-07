73ca2e70a9d046aa15b4c8b836d99ae135329604
Louafi Noureddine <noureddinelouafi8@gmail.com>
2021-10-07 19:32:22 +0100

    linked the express app with mongodb

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
+import stockRouter from "./routes";
+import config from "config";
 import { hostname, port } from "./utils/constants";
 
 const app = express();
 app.use(express.json());
 
-app.listen(port, hostname, () => console.log(`App listening on port ${port}`));
+app.use("/stock", stockRouter);
+
+mongoose
+  .connect(config.get("mongoUri"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to database successfully");
+    app.listen(port, hostname, () =>
+      console.log(`App listening on port ${port}`)
+    );
+  });
--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
 export const cities = ["Marseille", "Paris", "Dijon", "Nice", "Lille"];
 export const fruits = ["Orange", "Banane", "Pomme", "Fraise", "Cerise"];
-export const hostname = "localhost/v1";
+export const hostname = "localhost/api";
 export const port = process.env.PORT || 5000;
